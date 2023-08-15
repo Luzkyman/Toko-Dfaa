8dc2f59a3951dc24b74526f58640f8e155332fc5
Luzkyman <luqman.afif72@gmail.com>
2023-08-15 21:55:12 +0700

    Membuat Slider

--- app/src/main/java/com/example/tokodfaa/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/tokodfaa/fragment/HomeFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import com.example.tokodfaa.R
-
+import com.example.tokodfaa.adapter.AdapterSlider
 
 
 /**
@@ -14,11 +15,25 @@ import com.example.tokodfaa.R
  */
 class HomeFragment : Fragment() {
 
+    lateinit var vp_slider: ViewPager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        vp_slider = view.findViewById(R.id.vp_slider)
+
+        val arrSlider = ArrayList<Int>()
+        arrSlider.add(R.drawable.slider1)
+        arrSlider.add(R.drawable.slider2)
+        arrSlider.add(R.drawable.slider3)
+
+        val adapterSlider = AdapterSlider(arrSlider, activity)
+        vp_slider.adapter = adapterSlider
+
+        return view
     }
 }
